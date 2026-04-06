226af26166f23e26e0135327c2b09d7b22fcbb2a
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-04-06 20:08:14 +0530

    Django Model | Creating Superuser and Accessing Admin Panel;

---
+++ project_6/app_1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=20,null=True)
+    roll = models.IntegerField(primary_key=True)
+    address = models.TextField(null=True)
+    fathers_name = models.CharField(max_length=20,null=True)
+
+    def __str__(self):
+        return f"Roll No: {self.roll} - {self.name}"
