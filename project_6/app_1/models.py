3389b9ef31d7f6b35fa11d1ef98c488db1e9b9ce
Rakib Mondal <143431829+Rakib-Mondal-One8@users.noreply.github.com>
2026-04-07 16:36:25 +0530

    Showing Model data to frontend | Deleting Model data From frontend

--- project_6/app_1/models.py
+++ project_6/app_1/models.py
@@ -10,3 +10,12 @@ class Student(models.Model):
 
     def __str__(self):
         return f"Roll No: {self.roll} - {self.name}"
+    
+
+class Teacher(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+    def __self__(self):
+        return f"Name: {self.name},Age: {self.age}"
--- project_6/app_1/views.py
+++ project_6/app_1/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
+from . import models
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    data = models.Student.objects.all()
+    # data = models.Teacher.objects.all()
+
+    return render(request,'home.html',{'data':data})
+
+
+def delete_student(request,roll):
+    student = models.Student.objects.filter(roll=roll)
+    student.delete()
+
+    return redirect('home')
